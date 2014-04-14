86a51a0b774044c1597b614841b475c04212b852
Papo <mondinox@hotmail.com>
2014-04-14 17:31:58 -0300

    Hola esta es mi ventana

---
+++ venta/src/venta/ventanita.java
@@ -0,0 +1,25 @@
+package venta;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+
+public class ventanita {
+	
+	JButton bt1, bt2, bt3;
+
+	public static void main(String[] args) {
+		JFrame frame =new JFrame();
+		frame.setTitle("Mondino de Mondovi");
+		frame.setSize(800,600);
+		frame.setVisible(true);
+	
+		 
+		  
+
+
+		}
+	
+	}
+
+
